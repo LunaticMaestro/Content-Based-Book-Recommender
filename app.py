9062ce3881f00a3c65496d79046ad4a85cd6ed83
Deepak Sahu <38010046+LunaticMaestro@users.noreply.github.com>
2024-11-23 22:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-# from z_utils import get_dataframe
-# import numpy as np
-
-# # CONST
-# SUMMARY_VECTORS = "app_cache/summary_vectors.npy"
-# BOOKS_CSV = "clean_books_summary.csv"
+# CONST
+CLEAN_DF_UNIQUE_TITLES = "unique_titles_books_summary.csv"
+N_RECOMMENDS = 5
 
 # def get_recommendation(book_title: str) -> str:
 #     return book_title
@@ -31,22 +28,39 @@
 # Reference: https://huggingface.co/learn/nlp-course/en/chapter9/2
 
 import gradio as gr
-# from z_similarity import computes_similarity_w_hypothetical
+from z_similarity import computes_similarity_w_hypothetical
 from z_hypothetical_summary import generate_summaries
+from z_utils import get_dataframe
 
-def get_recommendation(book_title: str) -> str:
+books_df = get_dataframe(CLEAN_DF_UNIQUE_TITLES)
+
+
+def get_recommendation(book_title: str) -> dict:
     global generator_model
     # return "Hello"
     # # Generate hypothetical summary
     # value = generator_model("hello", max_length=50)
     fake_summaries = generate_summaries(book_title=book_title, n_samples=5) # other parameters are set to default in the function
+    
+    # Compute Simialrity 
+    similarity, ranks = computes_similarity_w_hypothetical(hypothetical_summaries=fake_summaries)
+    
+    # Get ranked Documents 
+    df_ranked =  books_df.iloc[ranks]
+    df_ranked = df_ranked.reset_index()
+    
+    books = df_ranked["book_name"][:N_RECOMMENDS]
+    scores = similarity[ranks][:N_RECOMMENDS]
+
+    return {book: score for book, score in zip(books, scores)} # referene: https://huggingface.co/docs/hub/en/spaces-sdks-gradio
+
     return fake_summaries[0]
     # return str(value)
 
 # We instantiate the Textbox class
-textbox = gr.Textbox(label="Write truth you wana Know:", placeholder="John Doe", lines=2)
+textbox = gr.Textbox(label="Write random title", placeholder="The Man who knew", lines=2)
+label = gr.Label(label="Result", num_top_classes=N_RECOMMENDS)
 
-
-demo = gr.Interface(fn=get_recommendation, inputs=textbox, outputs="text")
+demo = gr.Interface(fn=get_recommendation, inputs=textbox, outputs=label)
 
 demo.launch()
